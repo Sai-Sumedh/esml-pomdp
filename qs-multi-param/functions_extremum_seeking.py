6676c1d297adc4e87f214937a018d61771bc5fbd
H R Sai Sumedh <52331501+Sai-Sumedh@users.noreply.github.com>
2023-06-12 20:25:27 +0530

    Add files via upload

---
+++ qs-multi-param/functions_extremum_seeking.py
@@ -0,0 +1,115 @@
+import numpy as np
+from functions_queueing_system import *
+
+def update_omega(omega_bar, b_t, w0_bar):
+    return omega_bar+b_t*w0_bar #linear increase instead of exponential
+
+
+def update_theta_es(theta_bar_old, dist_shape_list, a_t, omega_bar_t, pi_t, ot_ind, numvals_z, theta_bar_indices, dep_rate,\
+                    theta_bar_limits, M, K, gamma_bar=np.array([0.1, 0.1]), unsymmetric=False):
+    """
+    Includes a departure rate (prob of departure), and momentum
+    No errors, but correctness not checked (tentative Checked OK)
+    """
+    pert_vector = np.sin(omega_bar_t)
+    theta_bar_pert = theta_bar_old + gamma_bar*pert_vector #elementwise product
+    dist_pert_list = update_dist(theta_bar_pert, dist_shape_list, theta_bar_limits, theta_bar_indices, M, unsymmetric)
+    p_th_pert = get_full_dist(dist_pert_list)
+        
+    p_th_pert_xn = np.sum(np.sum(p_th_pert[:, ot_ind, :, :], axis=0)*np.array([1-dep_rate[ot_ind],\
+                                                                               dep_rate[ot_ind]]).reshape((1,-1)),axis=1)
+    p_th_pert_cond = np.sum(pi_t.reshape((-1,))*p_th_pert_xn.reshape((-1,))) #p(O_{t}\mid O^{t-1})
+    theta_bar_new = theta_bar_old + (a_t/gamma_bar)*pert_vector*np.log(p_th_pert_cond)
+    thr = 1e-5 #threshold- to limit theta to (thr, 1-thr)
+
+    for i in range(len(theta_bar_new)): #restrict to corresponding limits
+        theta_bar_new[i] = min( max(theta_bar_new[i], theta_bar_limits[i][0]+thr), theta_bar_limits[i][1]-thr)
+    return theta_bar_new
+
+
+def estimate_theta_es(theta_bar_init, theta_bar_star, theta_bar_dim=2, M = 100, K = 50, T = 10000,\
+                      alpha=0.8, beta=0.7, w0_bar=[1,1/2], gamma_bar=[0.1,0.1], theta_bar_limits=[[0, 1],[0, 0.5]], tau=100, \
+                      dep_rate=[0.1, 0.7], A=0.01, B=10):
+    '''
+    no errors- OK working (correctness not checked)
+    '''
+    if theta_bar_dim==2:
+        unsymmetric = False
+    elif theta_bar_dim==3:
+        unsymmetric = True
+    else:
+        raise ValueError("Allowed values for theta_bar_dim are {2,3}")
+    
+    numvals_x = M+1
+    numvals_o = 2 #binary
+    numvals_z = 2
+    
+    X_qlen = np.zeros((T,))
+    O_obs = np.zeros((T,))
+    xi_arr = np.zeros((T,))
+    Z_dep = np.zeros((T,))
+    
+    #initialize
+    X_qlen[0] = np.random.randint(numvals_x)
+    O_obs[0] = (X_qlen[0]>=K) #just one point so can initialize any way
+    Z_dep[0] = np.random.randint(numvals_z)
+    xi_arr[0] = np.random.binomial(1,theta_bar_star[0])
+
+    # store all values of X, O, Z in arrays to easily retrieve the index later on
+    x_vals = np.arange(numvals_x) #all values of x
+    o_vals = np.arange(numvals_o)
+    z_vals = np.arange(numvals_z)
+
+    pi_t = np.zeros((numvals_x,))
+    pi_t[:] = 1/(numvals_x) #start with a uniform belief state (distribution over latent states)
+    pi_t_all = np.zeros((numvals_x, T))
+    pi_t_all[:,0] = pi_t[:]
+
+    theta_bar_hat_t = np.zeros((theta_bar_dim, T)) #2-dimensional estimate through time
+    
+    ######################################## INITIAL ESTIMATE OF THETA
+    theta_bar_hat_t[:,0]= theta_bar_init # a random initial guess of the unknown parameter
+    
+    state_dist_shape = (numvals_x, numvals_x, numvals_z) #p(X_{n+1} | X_n, Z_n)
+    obs_dist_shape = (numvals_o, numvals_x) #p(O_{n+1} | X_{n+1})
+    dist_shape_list = [state_dist_shape, obs_dist_shape]
+    theta_indices, theta_not_indices = get_nonzero_indices_state(state_dist_shape, M)
+    ind_obs = get_nonzero_indices_obs(obs_dist_shape, K, unsymmetric)
+    if not unsymmetric:
+        phi_indices, phi_not_indices = ind_obs[0], ind_obs[1]
+        theta_bar_indices = [(theta_indices, theta_not_indices), (phi_indices, phi_not_indices)]
+    else:
+        phi_indices, phi_not_indices, zeta_indices, zeta_not_indices = ind_obs[0][0], ind_obs[1][0], ind_obs[0][1], ind_obs[1][1]
+        theta_bar_indices = [(theta_indices, theta_not_indices), (phi_indices, phi_not_indices), (zeta_indices, zeta_not_indices)]
+    dist_list = update_dist(theta_bar_hat_t[:,0], dist_shape_list, theta_bar_limits, theta_bar_indices, M, unsymmetric)
+
+    omega_bar_t = np.zeros((theta_bar_dim,)) #initial value
+    omega_bar_t_all = np.zeros((theta_bar_dim ,T))
+    omega_bar_t_all[:,0]=omega_bar_t
+    
+    for t in range(1,T):
+        #dynamics
+        xi_arr[t] = np.random.binomial(1, theta_bar_star[0]) #with prob theta
+        X_qlen[t], O_obs[t] = update_X_O(X_qlen[t-1], Z_dep[t-1], xi_arr[t], M, K, theta_bar_star, unsymmetric)
+        #control policy
+        Z_dep[t] = (1-O_obs[t])*np.random.binomial(1, p=dep_rate[0]) + (O_obs[t])*np.random.binomial(1, p=dep_rate[1])
+        ztmin1_ind, ot_ind = get_z_o_indices(Z_dep[t-1], O_obs[t], z_vals, o_vals)
+        
+        #algorithm
+        #omega_update
+        tred = math.ceil(t/tau)
+        a_t = A/tred**alpha
+        b_t = B/tred**beta
+        omega_bar_t=update_omega(omega_bar_t, b_t, w0_bar)
+        omega_bar_t_all[:,t]=omega_bar_t
+        #theta-update
+        theta_bar_hat_t[:,t] = update_theta_es(theta_bar_hat_t[:,t-1], dist_shape_list, a_t, omega_bar_t, pi_t, ot_ind, \
+                                             numvals_z, theta_bar_indices, dep_rate, theta_bar_limits, M, K, gamma_bar, unsymmetric) 
+        dist_list = update_dist(theta_bar_hat_t[:,t], dist_shape_list, theta_bar_limits, theta_bar_indices, M, unsymmetric)
+        #pi-update
+        pi_t = update_pi(pi_t, dist_list, ot_ind, numvals_z, dep_rate=dep_rate)
+        pi_t_all[:,t] = pi_t[:]
+        
+    results = {'X':X_qlen, 'O':O_obs, 'xi':xi_arr, 'Z':Z_dep, 'pi_t_all':pi_t_all, 'theta_hat_t':theta_bar_hat_t,\
+               'omega_t':omega_bar_t_all, 'dist_list':dist_list}
+    return results
---
+++ qs-multi-param/functions_queueing_system.py
@@ -0,0 +1,180 @@
+import numpy as np
+import math, time
+
+
+def get_nonzero_indices_state(state_dist_shape, M): #for p(Xn | X_{n-1}, Z_{n-1})
+    '''
+    Checked OK 
+    '''
+    theta_indices = np.zeros(state_dist_shape, dtype=bool) #location of 'p' in the transition probability kernel
+    theta_not_indices = np.zeros(state_dist_shape, dtype=bool) #location of '1-p' in the transition probability kernel
+    numvals_x = state_dist_shape[0]
+    numvals_z = state_dist_shape[2]
+    for xn in range(numvals_x):
+        for zn in range(numvals_z):
+            if not ((xn==M and zn==0) or (xn==0 and zn==1)):
+                f_x_z = xn- min(zn, xn)
+                theta_indices[f_x_z+1, xn, zn] = 1
+                theta_not_indices[f_x_z, xn, zn] = 1
+    return theta_indices, theta_not_indices
+
+def get_nonzero_indices_obs(obs_dist_shape, K, unsymmetric=False): #for p(On | Xn)
+    '''
+    Checked OK
+    '''
+    phi_indices = np.zeros(obs_dist_shape, dtype=bool)
+    phi_not_indices = np.zeros(obs_dist_shape, dtype=bool)
+    
+    zeta_indices = np.zeros(obs_dist_shape, dtype=bool)
+    zeta_not_indices = np.zeros(obs_dist_shape, dtype=bool)
+    
+    numvals_o = obs_dist_shape[0]
+    numvals_x = obs_dist_shape[1]
+    for xn in range(numvals_x):
+        if xn>=K:
+            phi_indices[0, xn] = 1
+            phi_not_indices[1, xn] = 1
+        else: #xn<K
+            if not unsymmetric:
+                phi_not_indices[0, xn] = 1
+                phi_indices[1, xn] = 1
+            else:
+                zeta_not_indices[0, xn] = 1
+                zeta_indices[1, xn] = 1
+                
+    if not unsymmetric:
+        return phi_indices, phi_not_indices
+    else:
+        return [(phi_indices, zeta_indices), (phi_not_indices, zeta_not_indices)]
+
+
+def get_full_dist(dist_list):
+    '''
+    Checked OK
+    '''
+    state_dist, obs_dist = dist_list[0], dist_list[1]
+    numvals_o = obs_dist.shape[0]
+    numvals_x = obs_dist.shape[1]
+    numvals_z = state_dist.shape[2]
+    dist = np.zeros((numvals_x, numvals_o, numvals_x, numvals_z))
+    for on in range(numvals_o):
+        dist[:,on,:,:] = state_dist[:,:,:]*(obs_dist[on,:].reshape((-1,1,1)))
+    return dist
+
+
+def update_dist(theta_bar_new, dist_shape_list, theta_bar_limits, theta_bar_indices, M, unsymmetric=False):
+    """
+    dist_shape_list: 2 entries: state_dist_shape (tuple), obs_dist_shape (tuple)
+    theta_bar_new: list with 2 entries: theta and phi
+    theta_bar_indices: list with 2 (or 3) entries: (theta_indices, theta_not_indices), (phi_indices, phi_not_indices), (zeta_indices, zeta_not_indices)
+    theta_bar_limits: [(theta_min, theta_max), (phi_min, phi_max)]
+    
+    Idea: pre-identify indices where p appears and where 1-p appears
+    to update the transition kernel, just update the saved indices 
+    directly instead of looping over XZ values again (most are zeroes)
+    
+    Checked OK
+    """
+    state_dist_new = np.zeros(dist_shape_list[0])
+    obs_dist_new = np.zeros(dist_shape_list[1])
+    dist_list_new = [state_dist_new, obs_dist_new]
+    thr = 1e-5 #threshold
+    for i in range(len(theta_bar_new)): #restrict to corresponding limits
+        theta_bar_new[i] = min( max(theta_bar_new[i], theta_bar_limits[i][0]+thr), theta_bar_limits[i][1]-thr)
+    
+    dist_list_new[0][theta_bar_indices[0][0]] = theta_bar_new[0] #theta
+    dist_list_new[0][theta_bar_indices[0][1]] = 1-theta_bar_new[0] #theta_not (1-theta)
+    dist_list_new[0][M, M, 0] = 1 #at terminal state
+    dist_list_new[0][0, 0, 1] = 1 #at 0-state, with departure=1, Xnew = 0-1+arr (will be 0 irrespective of arr) 
+    
+    dist_list_new[1][theta_bar_indices[1][0]] = theta_bar_new[1] #phi
+    dist_list_new[1][theta_bar_indices[1][1]] = 1-theta_bar_new[1] #phi_not (1-phi)
+    
+    if unsymmetric: #another parameter
+        dist_list_new[1][theta_bar_indices[2][0]] = theta_bar_new[2] #zeta
+        dist_list_new[1][theta_bar_indices[2][1]] = 1-theta_bar_new[2] #zeta_not (1-zeta)
+        
+    
+    return dist_list_new
+
+
+def update_pi(pi_curr, dist_list, o_new_ind, numvals_z, dep_rate):
+    """
+    OK working (correctness not checked)
+    """
+    p_theta = get_full_dist(dist_list)
+    p_theta_reduced = np.sum(p_theta[:, o_new_ind, :, :]*np.array([1-dep_rate[o_new_ind], \
+                                                                       dep_rate[o_new_ind]]).reshape((1,1,-1)), axis=-1).T
+    prod_pi_p_red = pi_curr.reshape((1,-1)) @ p_theta_reduced
+    pi_new = prod_pi_p_red / np.sum(prod_pi_p_red)
+    pi_new = np.squeeze(pi_new)
+    return pi_new
+
+def update_X_O(xk, zk, xikpl1, M, K, theta_bar_star, unsymmetric=False): #stochastic observations now
+    xtemp = xk-min(zk,xk) + xikpl1
+    x_new = min(xtemp, M)
+    # o_new = (x_new>=K)
+    if x_new>=K:
+        o_new = np.random.binomial(n=1, p=1-theta_bar_star[1]) #phi
+    else:
+        if unsymmetric:
+            o_new = np.random.binomial(n=1, p=theta_bar_star[2]) #zeta
+        else:
+            o_new = np.random.binomial(n=1, p=theta_bar_star[1])
+    return x_new, o_new
+
+
+def get_z_o_indices(zk, ok, z_vals, o_vals):
+    zind = int(np.argwhere(z_vals==zk))
+    oind = int(np.argwhere(o_vals==ok))
+    return zind, oind
+
+
+def compute_ll_next(ll_prev, pi_current, dist_list_current, dep_rate, o_new_ind):
+    p_theta_current = get_full_dist(dist_list_current)
+    # p_theta_reduced = p_theta_current[:, o_new_ind, :, u_old_ind].T #shape xn,xnpl1
+    p_theta_reduced = np.sum(p_theta_current[:, o_new_ind, :, :]*np.array([1-dep_rate[o_new_ind], \
+                                                                       dep_rate[o_new_ind]]).reshape((1,1,-1)), axis=-1).T
+    prod = pi_current.reshape((-1,1))*p_theta_reduced
+    ll_next = ll_prev+np.log(np.sum(prod))
+    return ll_next
+
+
+def compute_log_likelihood_alldata(theta_bar_estimate, O_t, T, z_vals, o_vals, x_vals, K, theta_bar_limits, \
+                                   saveLLt=False, dep_rate=None, unsymmetric=False):
+    LL = 0
+    numvals_x = len(x_vals)
+    numvals_z = len(z_vals)
+    numvals_o = len(o_vals)
+    M = numvals_x-1 #largest value of X
+    # p_theta = np.zeros((numvals_x, numvals_o, numvals_x, numvals_z))
+    # theta_indices, theta_not_indices = get_nonzero_indices_p_theta(p_theta.shape, numvals_x, numvals_z, M, K)
+    # p_theta = compute_p_theta(p_theta, theta_estimate, theta_indices, theta_not_indices, M, K)
+    state_dist_shape = (numvals_x, numvals_x, numvals_z) #p(X_{n+1} | X_n, Z_n)
+    obs_dist_shape = (numvals_o, numvals_x) #p(O_{n+1} | X_{n+1})
+    dist_shape_list = [state_dist_shape, obs_dist_shape]
+    theta_indices, theta_not_indices = get_nonzero_indices_state(state_dist_shape, M)
+    indices_obs_params = get_nonzero_indices_obs(obs_dist_shape, K, unsymmetric)
+    if not unsymmetric:
+        phi_indices, phi_not_indices = indices_obs_params[0], indices_obs_params[1]
+        theta_bar_indices = [(theta_indices, theta_not_indices), (phi_indices, phi_not_indices)]
+    else:
+        phi_indices, phi_not_indices = indices_obs_params[0][0], indices_obs_params[1][0]
+        zeta_indices, zeta_not_indices = indices_obs_params[0][1], indices_obs_params[1][1]
+        theta_bar_indices = [(theta_indices, theta_not_indices), (phi_indices, phi_not_indices), (zeta_indices, zeta_not_indices)]
+    dist_list = update_dist(theta_bar_estimate, dist_shape_list, theta_bar_limits, theta_bar_indices, M, unsymmetric)
+    
+    pi_t = np.zeros((numvals_x,))
+    pi_t[:] = 1/(numvals_x) #start with a uniform belief state (distribution over latent states)
+    LL_t = np.zeros((T,))
+    for t in range(1,T):
+        ot_ind = int(np.argwhere(o_vals==O_t[t]))
+        LL = compute_ll_next(ll_prev=LL, pi_current=pi_t, dist_list_current=dist_list, dep_rate=dep_rate, o_new_ind=ot_ind)
+        pi_t = update_pi(pi_t, dist_list, ot_ind, numvals_z, dep_rate=dep_rate)
+        LL_t[t] = LL
+    if saveLLt:
+        return LL_t
+    else:
+        return LL
+
+
